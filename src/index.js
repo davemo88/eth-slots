841ca349759c53e43f7a4452064ebe808d988e47
whatever <matt@svv.im>
2018-03-08 20:29:15 -0500

    :cherries:

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-function bet() {
+import {c} from './contract-info.json';
+
+() => {
+var Web3 = require('web3');
+
+if (typeof web3 !== 'undefined') {
+  web3js = new Web3(web3.currentProvider);
+} else {
+  throw "I don't care. I don't care. I don't care. I don't care. No one said life was easy."
 }
 
+let ACCOUNT = web3.accounts[0];
+};
+
+/**
+ * Spin the wheel
+ * @param {nothing} nothing
+ */
 function spin() {
+  console.log(ACCOUNT);
 }
 
 export {
+  spin,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     publicPath: "assets/",
     filename: 'eth-slots.bundled.js',
     libraryTarget: 'var',
-    library: 'dd',
+    library: 'slots',
   },
   module: {
     loaders: [
