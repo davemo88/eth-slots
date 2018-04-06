4c63141187bb5f556bc69b315e07e7901797bd62
dk <davidkasofsky@gmail.com>
2018-04-05 20:19:04 -0400

    fixed test units #emojis

--- tests/slots-test.js
+++ tests/slots-test.js
@@ -32,38 +32,43 @@ contract('Slots', function (accounts) {
     let SPIN_VALUE = web3.toWei(0.1, "ether"); 
     let SPIN_NUMBER = new big.BigNumber(SPIN_VALUE);
 
-
     let ALICE_OLD_BALANCE = await web3.eth.getBalance(ALICE);
+    ALICE_OLD_BALANCE = web3.fromWei(ALICE_OLD_BALANCE, 'ether');
 
     let res1 = await deployed.spin({from: ALICE, value: SPIN_VALUE});
 
+    let ALICE_NEW_BALANCE = await web3.eth.getBalance(ALICE);
+    ALICE_NEW_BALANCE = web3.fromWei(ALICE_NEW_BALANCE, 'ether');
 
-    ALICE_NEW_BALANCE = await web3.eth.getBalance(ALICE);
+    SPIN_BIG_NUMBER = web3.fromWei(SPIN_NUMBER, 'ether');
 
+    GAS_USED = res1.receipt.cumulativeGasUsed;
+    GAS_PRICE = web3.fromWei(100000000000, 'ether');
+    GAS_COST = (GAS_USED * GAS_PRICE).toFixed(8);
+//    GAS_COST = new big.BigNumber(GAS_USED * GAS_PRICE);
 
     console.log("===============================");
-    console.log(ALICE_OLD_BALANCE.minus(ALICE_NEW_BALANCE).toString(10));
-    console.log(SPIN_NUMBER.plus(web3.toWei(res1.receipt.cumulativeGasUsed,"shannon")).toString(10));
-    return;
+    console.log("Alice balance before spin: ", ALICE_OLD_BALANCE.toString(10));
+    console.log("Alice balance after spin:  ", ALICE_NEW_BALANCE.toString(10));
+    console.log("");
+    console.log("Spin Amount: ", SPIN_BIG_NUMBER.toString(10));
+    console.log("Gas Cost: ", GAS_COST);
+
     console.log(
       ALICE_OLD_BALANCE.toString(10),
       "=",
       ALICE_NEW_BALANCE.toString(10),
       "+",
-      SPIN_VALUE.toString(10),
+      SPIN_BIG_NUMBER.toString(10),
       "+",
-      MISSING.toString(10),
+      GAS_COST.toString(10),
       "=",
+      ALICE_NEW_BALANCE.plus(SPIN_BIG_NUMBER).plus(GAS_COST).toString(10)
     );
+
+    return;
+
     console.log(ALICE_NEW_BALANCE.plus(SPIN_VALUE).plus(res1.receipt.gasUsed).toString(10));
-
-
-    // ...
-    // console.log(await web3.eth.getBalance(ALICE).toString(10));
-    // console.log(await web3.eth.getBalance(ALICE).toString(10));
-    // console.log(web3.eth.gasPrice.toString(10));
-    // console.log("<<<", (await deployed.getBalance.call()).toString(10));
-    // console.log("OUR BALANCE #2 =", await web3.eth.getBalance(contractAddress).toString(10));
 
   });
 
